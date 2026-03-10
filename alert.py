6013549d302312ea16b5bc622c0772d98b380250
Praveen <123853250+praveenxo-dev@users.noreply.github.com>
2026-03-10 18:32:00 +0530

    add requirement filE

--- alert.py
+++ alert.py
@@ -13,7 +13,7 @@ def apply_conditional_formatting():
     green_fill = PatternFill(patternType="solid", fgColor="00FF00")
 
     # Loop through cells
-    for row in ws.iter_rows(min_row=2, min_col=1, max_col=6):
+    for row in ws.iter_rows(min_row=2, min_col=1, max_col=9):
         for cell in row:
             if isinstance(cell.value, (int, float)):
                 if cell.value > 15:
@@ -26,3 +26,5 @@ def apply_conditional_formatting():
                     cell.fill = green_fill
 
     wb.save(file_path)
+
+apply_conditional_formatting()
