acb3aa0282321c1a834e19c3ed615bb10ce6809e
Praveen <123853250+praveenxo-dev@users.noreply.github.com>
2026-03-18 18:32:05 +0530

    fix the formate

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 
 import pandas as pd
 from dotenv import load_dotenv
+from get_excel import get_excel
 
 from alert import apply_conditional_formatting
 from extract_data import extract_data
@@ -17,18 +18,14 @@ ACTION_EXCEL = os.getenv("ACTION_EXCEL")
 
 def main():
     start_time = time.time()
-    # get_excel(RTR_URL, "data/Mobileum - CRP Tracker -RTR.xlsx")
-    # get_excel(P2P_URL, "data/Mobileum  - CRP Tracker - P2P.xlsx")
-    # get_excel(ACTION_EXCEL, "data/Mobileum.xlsx")
+    get_excel(RTR_URL, "data/Mobileum - CRP Tracker -RTR.xlsx")
+    get_excel(P2P_URL, "data/Mobileum  - CRP Tracker - P2P.xlsx")
+    get_excel(ACTION_EXCEL, "data/Mobileum.xlsx")
     print(f"Excel files downloaded in {time.time() - start_time:.2f} seconds.")
     time.sleep(2)  # Optional: Wait for a moment to ensure files are fully saved
 
-    df = pd.read_excel(
-        "data/Mobileum  - CRP Tracker - P2P.xlsx", sheet_name="CRP Action Items"
-    )
-    df1 = pd.read_excel(
-        "data/Mobileum - CRP Tracker -RTR.xlsx", sheet_name="Action Items - CRP"
-    )
+    df = pd.read_excel("data/Mobileum  - CRP Tracker - P2P.xlsx", sheet_name="CRP Action Items")
+    df1 = pd.read_excel("data/Mobileum - CRP Tracker -RTR.xlsx", sheet_name="Action Items - CRP")
     pivot_df = pd.read_excel("data/Mobileum.xlsx", sheet_name="Pivot")  # bottom table
 
     # Write first table (top)
@@ -43,14 +40,6 @@ def main():
         mode="a",
         if_sheet_exists="overlay",
     ) as writer:
-        pivot_df.to_excel(
-            writer,
-            sheet_name="Sheet1",
-            startcol=0,
-            startrow=8,
-            index=False,
-            header=False,
-        )
         summary_df.to_excel(
             writer,
             sheet_name="Sheet1",
@@ -59,11 +48,21 @@ def main():
             index=False,
             header=False,
         )
+        apply_conditional_formatting()
+        pivot_df.to_excel(
+            writer,
+            sheet_name="Sheet1",
+            startcol=0,
+            startrow=8,
+            index=False,
+            header=False,
+        )
+
     print(
         f"Summary data written to Excel file successfully in "
         f"{time.time() - start_time:.2f} seconds."
     )
-    apply_conditional_formatting()
+
     print(f"Conditional formatting applied in {time.time() - start_time:.2f} seconds.")
     print("Excel file updated with summary data and conditional formatting applied.")
     export_excel_to_image(
